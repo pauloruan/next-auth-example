df293c7bae09ecc4e2fdd9fab3ed8d32e71e985f
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-08 17:22:04 -0300

    feat: configured images component

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
       { loader: 'next/font/google', options: { subsets: ['latin'] } },
     ],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
